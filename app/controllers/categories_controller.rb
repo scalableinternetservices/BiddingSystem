cb48650cbfb5ff95ffb960ccd02227a05cfe7574
Seemanta Saha <seemantasaha@gmail.com>
2016-10-21 03:21:14 +0000

    Implementation of pagination

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,8 @@ class CategoriesController < ApplicationController
   # GET /categories
   # GET /categories.json
   def index
-    @categories = Category.all
+    @categories = Category.paginate(page: params[:page], per_page: 5)
+    
   end
 
   # GET /categories/1
