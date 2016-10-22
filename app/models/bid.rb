760f379e3ccdf196a45dbb4ec2baa3605c2addde
Sanjana Sahayaraj <sanjana@umail.ucsb.edu>
2016-10-22 17:15:37 +0000

    buyer controller actions

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,2 +1,30 @@
 class Bid < ApplicationRecord
+  self.primary_key='bid_id'
+  
+  def self.place_bid?(product_id,user_id,bid_amount)
+    bidding_date = Date.today.to_formatted_s(:date)
+    bidding_time = Time.zone.now.to_formatted_s(:time)
+    bid = Bid.new(product_id: product_id,user_id: user_id,bid_amount: bid_amount, bidding_date: bidding_date, bidding_time: bidding_time)
+    bid.save
+  end
+  
+  def self.revoke_bid?(product_id,user_id)
+    bid=Bid.where(user_id => user_id, product_id => product_id).last
+    bid.delete
+  end
+  
+  def self.get_highest_bid?(product_id)
+    highbid=Bid.order(bid_amount: :desc).offset(0)
+    highbid.show
+  end
+  
+  def self.get_all_bids?(product_id)
+    list=Bid.where(product_id => product_id)
+    return list
+  end
+  
+  def self.get_bids_by_user?(user_id)
+    list=Bid.where(user_id => user_id)
+    return list
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
 
-  resources :bids, param: :bid_id
+  resources :bids, param: :bid_id do
+    member do
+      post 'place_bid'
+    end
+  end
   resources :categories, param: :category_id
   resources :products, param: :product_id
+
+  post '/bids/:bid_id', to: 'bids#show'
+
   
   # Application root
   root 'application#index'
