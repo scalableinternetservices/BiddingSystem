4c85cd5d47d6d62e58602aceee80bc6de4353876
PriyankaBose <babui.priyanka@gmail.com>
2016-10-27 20:15:06 +0000

    Fixed code in bid model and controller, added bid_active column in bid table
    Modified the place_bid and revoke_bid to return boolean values
    
    Code beautification

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -8,11 +8,13 @@ class BidsController < ApplicationController
   end
   
   def place_bid
-    product_id=params[:bid][:product_id]
-    user_id=params[:bid][:user_id]
-    bid_amount=params[:bid][:bid_amount]
+    
+    product_id = params[:bid][:product_id]
+    user_id = params[:bid][:user_id]
+    bid_amount = params[:bid][:bid_amount]
       
-    bid_placed=Bid.place_bid(product_id,user_id,bid_amount)
+    bid_placed = Bid.place_bid?(product_id, user_id, bid_amount)
+    
     if bid_placed
       respond_to do |format|
         format.html { redirect_to bid_url, notice: 'Bid was successfully placed.' }
@@ -22,41 +24,51 @@ class BidsController < ApplicationController
   end
     
   def revoke_bid
-    product_id=params[:bid][:product_id]
-    user_id=params[:bid][:user_id]
+    
+    product_id = params[:bid][:product_id]
+    user_id = params[:bid][:user_id]
       
-    bid_revoked=Bid.revoke_bid(product_id,user_id)
+    bid_revoked = Bid.revoke_bid?(product_id, user_id)
+    
     if bid_revoked
       respond_to do |format|
         format.html { redirect_to bid_url, notice: 'Bid was successfully destroyed.' }
         format.json { head :no_content }
       end
     end
+    
   end
     
   def get_all_bids
-    product_id=params[:bid][:product_id]
-      
-    @bidslist=Bid.get_all_the_bids(product_id)
-    @bidslist.each do |bid|
-      bid.show
+    
+    product_id = params[:bid][:product_id]
+    @bidslist = Bid.get_all_the_bids(product_id)
+    
+    respond_to do |format|
+      format.html { redirect_to @bidslist, notice: 'Products under bid was successfully created.' }
+      format.json { render json: @bidslist }
     end
+    
   end
     
   def get_bids_by_user
-    user_id=params[:bid][:user_id]
-      
-    @bidslist=Bid.get_bids_for_user(user_id)
-    @bidslist.each do |bid|
-      bid.show
+    
+    user_id = params[:bid][:user_id]
+    @bidslist = Bid.get_bids_for_user(user_id)
+    
+    respond_to do |format|
+      format.html { redirect_to @bidslist, notice: 'Products under bid was successfully created.' }
+      format.json { render json: @bidslist }
     end
+    
   end
 
   def show
-    @bid=Bid.find(params[:bid_id])
+    @bid = Bid.find(params[:bid_id])
   end
     
   private
+    
     def bid_params
       params.require(:bid).permit(:product_id, :user_id, :bid_amount, :bidding_date, :bidding_time)
     end
