7a8c5dc00f8f889b823f938a7916643605afaadd
Seemanta Saha <seemantasaha@gmail.com>
2016-11-02 23:58:29 +0000

    Pagination for searched and ongoing products

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,20 +1,26 @@
 class BuyersController < UsersController
     
-    def search
+    def show
+        @matching_products = ProductsUnderBid.search_products_under_bid(name: $search_name, category_id: $search_category, 
+                                                                        location_id: $search_location_id, highest_bid: $search_highest_bid)
+        @matching_products = @matching_products.paginate(:page => params[:page],:per_page => 10)
+        get_already_placed_bids(@matching_products)
     end
     
     def search_products
-        search_name = params[:search][:name]
-        search_category = params[:search][:category_id].to_i
-        search_location_id = params[:search][:location_id].to_i
-        search_highest_bid = params[:search][:highest_bid].to_f
-        @matching_products = ProductsUnderBid.search_products_under_bid(name: search_name, category_id: search_category, 
-                                                                        location_id: search_location_id, highest_bid: search_highest_bid)
+        $search_name = params[:search][:name]
+        $search_category = params[:search][:category_id].to_i
+        $search_location_id = params[:search][:location_id].to_i
+        $search_highest_bid = params[:search][:highest_bid].to_f
+        @matching_products = ProductsUnderBid.search_products_under_bid(name: $search_name, category_id: $search_category, 
+                                                                        location_id: $search_location_id, highest_bid: $search_highest_bid)
+        @matching_products = @matching_products.paginate(:page => params[:page],:per_page => 10)
         get_already_placed_bids(@matching_products)
     end
     
     def ongoing_auctions
         @products_under_bid = ProductsUnderBid.get_products_under_bid
+        @products_under_bid = @products_under_bid.paginate(:page => params[:page],:per_page => 10)
         get_already_placed_bids(@products_under_bid)
     end
     
