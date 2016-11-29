c9c8fcc92ce65af7dbddbd8c7763b0c55b881430
Seemanta Saha <seemantasaha@gmail.com>
2016-11-29 08:24:07 +0000

    Basic fragment caching

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,9 @@
 module CategoriesHelper
+    def cache_key_for_categories(category)
+        "category/#{category.category_id}/#{category.updated_at}"
+    end
+    
+    def cache_key_for_categories_show(category)
+        "category_show/#{category.category_id}/#{category.updated_at}"
+    end
 end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,13 @@
 module ProductsHelper
+    def cache_key_for_products(product)
+        "product/#{product.product_id}/#{product.updated_at}"
+    end
+    
+    def cache_key_for_products_show(product)
+        "product_show/#{product.product_id}/#{product.updated_at}"
+    end
+    
+    def cache_key_for_my_bids(product)
+        "my_bid/#{product.product_id}/#{product.updated_at}/#{product.maximum_bidding_price}"
+    end
 end
