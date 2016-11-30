7de662bbacde242d9d4d5f054cd4af3ee868ff3e
Priyanka Bose <babui.priyanka@gmail.com>
2016-11-30 10:35:48 +0000

    russian doll caching implemented

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -6,4 +6,8 @@ module CategoriesHelper
     def cache_key_for_categories_show(category)
         "category_show/#{category.category_id}/#{category.updated_at}"
     end
+    
+    def cache_key_for_categories_table(category, page)
+        "category_table/#{page}/#{Category.maximum(:updated_at)}/#{Category.count}"
+    end
 end
--- app/helpers/products_under_bids_helper.rb
+++ app/helpers/products_under_bids_helper.rb
@@ -1,2 +1,5 @@
 module ProductsUnderBidsHelper
+    def cache_key_for_my_bids_table(products)
+        "my_bids_table/#{current_user.id}/#{products.maximum(:updated_at)}/#{products.count}"
+    end
 end
